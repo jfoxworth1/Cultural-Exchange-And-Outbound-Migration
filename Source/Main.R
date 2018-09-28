f5c90c10d0410be9c292466296921871266a1f38
Joshua Foxworth <jfoxworth1@gmail.com>
2018-09-28 11:45:33 +0200

    Add main file with small additions

---
+++ Source/Main.R
@@ -0,0 +1,15 @@
+# List Dependencies
+require(tidyverse)
+
+# list important directories
+MainDir <- "/home/me/Documents/Uni-Jena/Thesis/Cultural-Exchange-And-Outbound-Migration/"
+WorkDir <- paste0(MainDir, "Source")
+DataDir <- paste0(MainDir, "Data")
+
+# download data into memory
+setwd(DataDir)
+
+
+
+# Return to WorkDir
+setwd(WorkDir)
