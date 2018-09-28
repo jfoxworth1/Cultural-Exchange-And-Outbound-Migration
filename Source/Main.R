65909bde3a4dcb4f746a9fe4770439dbade7f23a
jfoxworth1 <jfoxworth1@gmail.com>
2018-09-28 12:14:58 +0200

    Data additions

--- Source/Main.R
+++ Source/Main.R
@@ -8,8 +8,12 @@ DataDir <- paste0(MainDir, "Data")
 
 # download data into memory
 setwd(DataDir)
-
-
+files <- list.files(pattern = ".csv$")
+for(i in seq_along(files)){
+  assign(str_remove(files[i], ".csv"), read_csv(files[i]))
+}
+data_oecd <- MIG_16092018154825543
+rm(MIG_16092018154825543, files)
 
 # Return to WorkDir
 setwd(WorkDir)
