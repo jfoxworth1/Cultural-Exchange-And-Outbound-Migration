692625c709ee8dba303cf672887424eb83680383
jfoxworth1 <jfoxworth1@gmail.com>
2018-10-03 08:46:13 +0200

    Code commit

--- Source/Main.R
+++ Source/Main.R
@@ -11,24 +11,22 @@ setwd(DataDir)
 files <- list.files(pattern = ".csv$")
 for(i in seq_along(files)){
   object_name <- str_remove(files[i], ".csv")
-  assign(object_name, read_csv(files[i]))
-  tmp_names <- toupper(colnames(get(object_name)))
- # colnames(eval(parse(text = object_name))) <- tmp_names
+  tmp <- read_csv(files[i])
+  names(tmp) <- tolower(colnames(tmp))
+  assign(object_name, tmp)
 }
 data_oecd <- MIG_16092018154825543
-rm(MIG_16092018154825543, files)
+rm(MIG_16092018154825543, files, tmp, object_name)
 
 # Return to WorkDir
 # Build combined data frame
 setwd(WorkDir)
-names(data_oecd)
 names(data_oecd)[1] <- "country_iso3"
-names(data_oecd)[10] <- "year"
 names(data_oecd)[2] <- "nationality" 
 data_joined <- right_join(data_oecd, data_abroad) 
 data_joined <- right_join(data_joined, data_presence)
-data_joined <- filter(data_joined, !is.na(VAR))
-#data_joined <- filter(data_joined, )
+data_joined <- filter(data_joined, !is.na(var))
+data_joined <- filter(data_joined, nationality = "Germany")
 rm(data_abroad, data_ger, data_presence, data_oecd)
 
 # Build list of Countries
