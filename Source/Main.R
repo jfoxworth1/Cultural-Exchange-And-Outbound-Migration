ebb12c8b7ba1819ee53117333100b99294acad99
jfoxworth1 <jfoxworth1@gmail.com>
2018-10-04 15:47:39 +0200

    Added economic data to use as a control and added that to the analysis

--- Source/Main.R
+++ Source/Main.R
@@ -1,5 +1,6 @@
 # List Dependencies
 require(tidyverse)
+require(tseries)
 
 # list important directories
 MainDir <- "/home/me/Documents/Uni-Jena/Thesis/Cultural-Exchange-And-Outbound-Migration/"
@@ -15,32 +16,72 @@ for(i in seq_along(files)){
   names(tmp) <- tolower(colnames(tmp))
   assign(object_name, tmp)
 }
-data_oecd <- MIG_16092018154825543
-rm(MIG_16092018154825543, files, tmp, object_name)
+rm(files, tmp, object_name)
 setwd(WorkDir)
 
-# Build combined data frame
-# Data is the amount of germans that flow into other countries
-# Limit OECD data to necessary bits
+# Combine Data about OECD countries and trim out unnecesary parts
+names(OECD_GDP)[1] <- "country_iso3"
+names(OECD_GDP)[6] <- "year" 
+names(OECD_GDP)[7] <- "GDP_USD_CAP"
+OECD_GDP <- select(OECD_GDP, country_iso3, year, GDP_USD_CAP)
+
+Germany_GDP <- filter(OECD_GDP, country_iso3 == "DEU")
+names(Germany_GDP)[3] <- "Germany_GDP_USD_CAP"
+Germany_GDP <- select(Germany_GDP, -country_iso3)
+
+OECD_Employment_Rates <- OECD_Employment_Rates[c(-6, -9,-15)]
+names(OECD_Employment_Rates)[1] <- "country_iso3"
+names(OECD_Employment_Rates)[6] <- "rate_code"
+names(OECD_Employment_Rates)[14] <- "Migrant_Employment_Rate"
+OECD_Employment_Rates <- filter(OECD_Employment_Rates, gender == "TOT",
+                                rate_code == "N_RATE",
+                                birth == "FB")
+OECD_Employment_Rates <- select(OECD_Employment_Rates, country_iso3, year, Migrant_Employment_Rate)
+
 names(data_oecd)[7] <- "country_iso3"
+names(data_oecd)[11] <- "Migration_Value"
 data_oecd <- filter(data_oecd, data_oecd$`country of birth/nationality` == "Germany")
 data_oecd <- filter(data_oecd, data_oecd$variable == "Inflows of foreign population by nationality")
 data_oecd <- select(data_oecd, -co2, -'country of birth/nationality', -var, -variable, -gen, -gender,-yea, -'flag codes', - flags)
 
+data_oecd <- right_join(OECD_GDP, data_oecd, by = c("country_iso3", "year"))
+data_oecd <- right_join(OECD_Employment_Rates, data_oecd, by = c("country_iso3", "year"))
+data_oecd <- right_join(Germany_GDP, data_oecd, by = "year")
+
+rm(OECD_GDP, OECD_Employment_Rates, Germany_GDP)
 # Create tables for the two different analysis
 # Based on Units
 data_abroad <- filter(data_abroad, data_abroad$country_iso3 %in% unique(data_oecd$country_iso3),
                       data_abroad$year >= min(data_oecd$year))
-data_unit_analysis <- right_join(data_oecd, data_abroad)
-data_unit_analysis <- filter(data_unit_analysis, !is.na(data_unit_analysis$value))
+data_unit_analysis <- right_join(data_oecd, data_abroad, by = c("year", "country_iso3"))
 
 # Based on Presence
 data_presence <- filter(data_presence, data_presence$country_iso3 %in% unique(data_oecd$country_iso3),
                         data_presence$year >= min(data_oecd$year),
                         data_presence$open1 >= min(data_oecd$year))
 data_presence_analysis <- right_join(data_oecd, data_presence)
-# remove unused data
+# Remove unused data
 rm(data_abroad, data_ger, data_presence, data_oecd)
 
 # Units Analysis
-early_value <- lm(value ~ units_sold + registrations +  exams, data = data_unit_analysis)
+early_value <- lm(Migration_Value ~ units_sold + GDP_USD_CAP + Germany_GDP_USD_CAP + Migrant_Employment_Rate, data = data_unit_analysis)
+
+
+
+
+
+
+
+
+# # Graph function
+#
+# ggplot(data = tmp_city_analysis) +
+#   geom_point(aes(x = year, y = units_sold, color = country))
+
+
+
+
+
+
+
+
